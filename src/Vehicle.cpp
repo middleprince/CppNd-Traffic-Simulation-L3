46635bb4dd3effb107de737a61445c5fce4d108b
middleprince <hellangel.ah@gmail.com>
2019-07-10 23:31:41 +0800

    feat: Address data racing problem

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -31,11 +31,14 @@ void Vehicle::simulate()
 // virtual function which is executed in a thread
 void Vehicle::drive()
 {
-    // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. 
+    // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you 
+    // have added to the base class TrafficObject in the previous task. 
 
     // print id of the current thread
-    std::cout << "Vehicle #" << _id << "::drive: thread id = " << std::this_thread::get_id() << std::endl;
+    std::unique_lock<std::mutex> lck(_mtxCout);
 
+    std::cout << "Vehicle #" << _id << "::drive: thread id = " << std::this_thread::get_id() << std::endl;
+    lck.unlock();
     // initalize variables
     bool hasEnteredIntersection = false;
     double cycleDuration = 1; // duration of a single simulation cycle in ms
